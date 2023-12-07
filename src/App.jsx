85b69fb4cc36a1fae2b0fe64688adfadbf0bb271
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-12-07 14:59:29 -0700

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import { useEffect, useState } from "react";
+// const imageUrl = "https://api.artic.edu/api/v1/artworks?ids=208131,240935,142595,120300,13454,151363,102611,191556,117266,137125,126414&fields=id,title,image_id";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Does It Work?</h1>
-    </div>
-  );
-}
+// function App() {
+//   const [img, setImg] = useState<string | undefined>(); 
 
-export default App;
+//   const fetchImage = async () => {
+//     const res = await fetch(imageUrl);
+//     const imageBlob = await res.blob();
+//     const imageObjectURL = URL.createObjectURL(imageBlob);
+//     setImg(imageObjectURL);
+//   };
+
+
+//   useEffect(() => {
+//     fetchImage();
+//   }, []);
+
+//   const classes = useStyles();
+
+//   return (
+//     <>
+//       <Navbar />
+//       <img src={img} alt="icons" />
+//       <div className={`${classes.background}`}>
+//         <div className={classes.main_text}>
+
+//           <div></div>
+//         </div>
+//       </div>
+      
+//     </>
+//   );
+// }
+
+// export default App;
