28e6bff40708abc8526602cbca330611871ea7de
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-12-07 14:03:35 -0700

    Set up pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Main = () => {
+  return (
+    <>
+        <Header/>
+        <div>Main</div>
+    </>
+    
+  )
+}
+
+export default Main
