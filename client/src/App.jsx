e718fe3edba0aa47beef4a8273778b646cc9ca25
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-12-14 15:18:07 -0700

    started implementing the AI

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator'
+const App = () => {
+  return (
+    <div>
+      <ImageGenerator/>
+    </div>
+  )
+}
+
+export default App
